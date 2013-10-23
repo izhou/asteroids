5c7fb58a5a52bf786103de9446c96dbf528b7dd2
App Academy Student <student@appacademy.io>
2013-10-23 11:51:20 -0700

    Add surrogate inheritance

---
+++ inheritance.js
@@ -0,0 +1,18 @@
+
+// Surrogate.prototype = SuperClass.prototype;
+// Subclass.prototype = new Surrogate();
+
+Function.prototype.inherits = function(superClass) {
+	function Surrogate() {};
+
+	Surrogate.prototype = superClass.prototype;
+	this.prototype = new Surrogate();
+}
+
+function Dog () {};
+Dog.prototype.bark = function () { console.log("Bark!"); };
+
+function Corgi () {};
+Corgi.inherits(Dog);
+
+new Corgi().bark();
